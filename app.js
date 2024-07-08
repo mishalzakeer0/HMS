b40288c7d773ce4de8093bb3e4ce8a308b331e48
Bititude Technologies <bititudetechnologies@Wolf.local>
2024-07-08 12:03:55 +0530

    all origin allow

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pool = require("./model/db_pool");
 const cors = require("cors");
 app.use(express.json());
 
-const allowedOrigins = ["https://668b81fee53931d70c508e78--hms1mz.netlify.app"];
+const allowedOrigins = ["https://668b87e5b074e9dd48f6f594--hms1mz.netlify.app"];
 app.use(cors({
     origin: function(origin, callback){
         // allow requests with no origin (like mobile apps or curl requests)
