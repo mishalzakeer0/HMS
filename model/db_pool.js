9250eae44736b760cbdd9661b35c6225f32df910
mishalzakeer0 <mishalzakeer0@gmail.com>
2024-07-25 16:05:00 +0530

    HF

--- model/db_pool.js
+++ model/db_pool.js
@@ -2,7 +2,6 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD, {
   host: process.env.MYSQL_ADDON_HOST, 
-  port: process.env.MYSQL_ADDON_PORT, 
   dialect: "mysql",
 });
 
